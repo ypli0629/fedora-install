ed2f7465a717cf68ecdd9c01078c04aa5ba1e79b
ypli0629 <liyapeng0629@gmail.com>
2024-10-09 14:45:31 +0800

    Add nvidia

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
+# upgrade
 sudo rpm-ostree upgrade -y
+# nvidia
 sudo rpm-ostree install akmod-nvidia xorg-x11-drv-nvidia-cuda -y #optional if using nvidia-smi or cuda
 sudo rpm-ostree kargs --append=rd.driver.blacklist=nouveau --append=modprobe.blacklist=nouveau --append=nvidia-drm.modeset=1 # this might not be needed at some point when ostree systems will support the standard way to specify this.
 
+# flatpak
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo flatpak remote-modify flathub --url=https://mirror.sjtu.edu.cn/flathub
 
-git clone https://github.com/ypli0629/astronvim_config.git ~/.config/nvim
-
+# gnome-shell
 sudo rpm-ostree install gnome-shell-extension-appindicator \ 
       gnome-shell-extension-auto-move-windows \ 
       gnome-shell-extension-blur-my-shell \
@@ -17,10 +19,36 @@ sudo rpm-ostree install gnome-shell-extension-appindicator \
       gnome-tweaks \
       -y
 
+# soft
 sudo rpm-ostree install autojump fcitx5-chinese-addons \ 
-      go neovim nodejs pnpm rustup zsh \
+      golang neovim nodejs pnpm rustup zsh flameshot wireshark \
       -y
 
+# gnome
+for i in $(seq 1 9); do
+	gsettings set org.gnome.shell.keybindings switch-to-application-${i} '[]'
+	gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-${i} "['<Shift><Super>"${i}"']"
+	gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-${i} "['<Super>"${i}"']"
+done
+
+gsettings set org.gnome.desktop.wm.keybindings toggle-fullscreen "['<Super>f']"
+gsettings set org.gnome.desktop.wm.keybindings close "['<Super>q']"
+gsettings set org.gnome.shell.keybindings toggle-application-view "['<Shift><Super>d']"
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'flameshot'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'flameshot gui'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Alt>F1'
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name 'gnome-terminal'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command 'gnome-terminal'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding '<Super>Return'
+
+# nvim
+git clone https://github.com/ypli0629/astronvim_config.git ~/.config/nvim
+
+# dir
 mkdir -p ~/Desktop/source
 mkdir -p ~/Desktop/work
 mkdir -p ~/Desktop/caffe
@@ -31,6 +59,7 @@ git config --global user.email "liyapeng0629@gmail.com"
 git config --global user.name "ypli0629"
 git config --global credential.helper store
 
+# zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 tee -a ~/.zshrc <<-'EOF'
 	alias szsh="source ~/.zshrc"
