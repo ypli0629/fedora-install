78832cfa0186de0f992255d8c9ac185153b6a0cc
ypli0629 <liyapeng0629@gmail.com>
2024-10-09 14:49:02 +0800

    Split nvidia

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
+#!/bin/bash
+
 # upgrade
 sudo rpm-ostree upgrade -y
-# nvidia
-sudo rpm-ostree install akmod-nvidia xorg-x11-drv-nvidia-cuda -y #optional if using nvidia-smi or cuda
-sudo rpm-ostree kargs --append=rd.driver.blacklist=nouveau --append=modprobe.blacklist=nouveau --append=nvidia-drm.modeset=1 # this might not be needed at some point when ostree systems will support the standard way to specify this.
 
 # flatpak
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
