bc5f16998d032faa0817592ab4d56a9478d18431
ypli0629 <liyapeng0629@gmail.com>
2024-10-09 15:15:17 +0800

    Add nvidia-gpu-firmware

--- nvidia.sh
+++ nvidia.sh
@@ -1,3 +1,3 @@
 # nvidia
-sudo rpm-ostree install akmod-nvidia xorg-x11-drv-nvidia-cuda -y #optional if using nvidia-smi or cuda
+sudo rpm-ostree install nvidia-gpu-firmware akmod-nvidia xorg-x11-drv-nvidia-cuda -y #optional if using nvidia-smi or cuda
 sudo rpm-ostree kargs --append=rd.driver.blacklist=nouveau --append=modprobe.blacklist=nouveau --append=nvidia-drm.modeset=1 # this might not be needed at some point when ostree systems will support the standard way to specify this.
