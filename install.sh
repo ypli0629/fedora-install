228529ec93c239770e6b9f1fd6289878651573e6
ypli0629 <liyapeng0629@gmail.com>
2024-10-09 14:24:25 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo rpm-ostree install autojump fcitx5-chinese-addons \ 
+      gnome-shell-extension-appindicator gnome-shell-extension-auto-move-windows gnome-shell-extension-blur-my-shell gnome-shell-extension-dash-to-dock
+                           gnome-shell-extension-netspeed gnome-shell-extension-unite
+                           gnome-shell-extension-user-theme gnome-tweaks go neovim nodejs
+                           pnpm rustup zsh
