2aa1eafae3882a9d51b8cc07f87d3f5025722c33
ypli0629 <liyapeng0629@gmail.com>
2024-10-12 13:52:46 +0800

    feat: add theme

---
+++ theme.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+mkdir themes
+
+cd themes
+git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
+cd WhiteSur-gtk-theme
+./install.sh -i fedora -l -N stable
+cd ..
+
+#git clone https://github.com/vinceliuice/grub2-themes.git
+#cd grub2-themes
+#sudo ./install.sh -t vimix -s 2k -b
+#cd ..
+
+git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git
+cd WhiteSur-icon-theme
+./install.sh -b
+cd ..
+
+git clone https://github.com/vinceliuice/WhiteSur-wallpapers.git
+cd WhiteSur-wallpapers
+./install-wallpapers.sh
+sudo ./install-gnome-backgrounds.sh
+cd ..
+
+git clone https://github.com/vinceliuice/WhiteSur-cursors.git
+cd WhiteSur-cursors
+sudo ./install.sh
+cd ..
