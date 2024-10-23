b4039e940338a094fd48bcec76d2095cfa991c8c
ypli0629 <liyapeng0629@gmail.com>
2024-10-23 03:02:01 -0500

    Update nvidia.sh

--- nvidia.sh
+++ nvidia.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # nvidia
-echo "GSK_RENDERER=ngl" > ~/.config/environment.d/gsk.conf
+mkdir -p `~/.config/environment.d`
+echo "GSK_RENDERER=gl" > ~/.config/environment.d/gsk.conf
 # sudo dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda -y #optional if using nvidia-smi or cuda
 #sudo dnf kargs --append=rd.driver.blacklist=nouveau --append=modprobe.blacklist=nouveau --append=nvidia-drm.modeset=1 # this might not be needed at some point when ostree systems will support the standard way to specify this.
